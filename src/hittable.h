e22437751df8bd945786b502f27dd9f6b884953f
Jay Tun <aungkyawtun.jay@gmail.com>
2025-04-26 21:14:21 +0200

    feat: remove header calls

--- src/hittable.h
+++ src/hittable.h
@@ -1,7 +1,6 @@
 #ifndef HITTABLE_H
 #define HITTABLE_H
 
-#include "ray.h"
 
 class hit_record{
  public:
@@ -22,7 +21,6 @@ class hittable{
 
     virtual bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const = 0;
 };
-
 
 
 #endif'
