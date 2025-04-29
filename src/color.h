78c8f58fe98d7827b0f37e0081c23aca905be3b5
Jay Tun <aungkyawtun.jay@gmail.com>
2025-04-29 20:12:25 +0200

    feat: gamma correction

--- src/color.h
+++ src/color.h
@@ -20,6 +20,11 @@ void write_color(std::ostream& out, const color& pixel_color){
     auto g = pixel_color.y();
     auto b = pixel_color.z();
 
+    //Apply linear to gamma 2 space transform
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
+
     //Translate [0,1] component values to byte range [0,255]
     static const interval intensity(0.000, 0.999);
     int rbyte = int(255.999 * intensity.clamp(r));
